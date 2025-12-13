faf75e4631e3a79e80392162e7a57f8af4188cd0
Yuzhou Nie <rucnyz@gmail.com>
2025-12-13 14:39:08 -0800

    Update test.sh with new API keys and model path

--- WebAgent/WebSailor/src/scripts/test.sh
+++ WebAgent/WebSailor/src/scripts/test.sh
@@ -1,11 +1,11 @@
-export JINA_API_KEY="your_jina_api_key"
 export SEARCH_API_URL="your_search_api_url"
-export GOOGLE_SEARCH_KEY="your_google_search_key"
+export JINA_API_KEY=jina_b76d29ac793941369e1e0cb6c8d78213PGYCi9gRj0CfegLFyXdGSay9wBnK
+export GOOGLE_SEARCH_KEY=475e6b446236268cd0fb9aa42a85b43cb94f2972
 
-export SUMMARY_MODEL_PATH="Qwen/Qwen2.5-72B-Instruct"
+export SUMMARY_MODEL_PATH="Qwen/Qwen2.5-32B-Instruct"
 export MAX_LENGTH=$((1024 * 31 - 500))
 
-cd src
+cd src || exit
 
 # The arguments are the model path, the dataset name, and the location of the prediction file.
 # bash run.sh <model_path> <dataset> <output_path>
@@ -16,4 +16,4 @@ cd src
 # - browsecomp_en (Full set, 1266 Cases)
 # - xbench-deepsearch
 
-bash run.sh Alibaba-NLP/WebSailor-3B gaia ../outputs
+bash run.sh Alibaba-NLP/WebSailor-3B gaia outputs
